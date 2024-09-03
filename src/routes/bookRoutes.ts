42237c1486a6ef21e03181e94d1a44fd354369ca
KTellezE <Kevin.Tellez@axity.com>
2024-09-03 17:16:08 -0600

    Datos de la BD

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -8,5 +8,5 @@ router.get("/books", bookController.getAll);
 router.post('/books', bookController.addBook);
 router.get('/books/:id', bookController.getById);
 router.delete('/books/:id', bookController.deleteBook);
-// router.put('/books/:id', bookController.update);
+router.put('/books/:id', bookController.update);
 export default router;
